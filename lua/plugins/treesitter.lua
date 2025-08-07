8e8a8a489b6f6bb42e1ddc58ed166d7b3196e462
Ryan Gibson <ryan@gibs.gg>
2025-08-07 15:43:03 -0500

    init commit

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  branch = 'master',
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      ensure_installed = { "c", "cpp", "cmake", "ruby", "lua", "javascript", "typescript", "json", "liquid" },
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
+}
+
