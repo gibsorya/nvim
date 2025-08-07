68f973c571b38a8579138aa7d7d169496653d84a
Ryan Gibson <ryan@gibs.gg>
2025-08-07 16:39:48 -0500

    move options and keymaps to separate files

--- init.lua
+++ init.lua
@@ -1,8 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set autoindent")
-vim.g.mapleader = " "
-
-require("config.lazy")
+require("config/lazy")
+require("config/options")
+require("config/keymaps")
---
+++ lua/config/options.lua
@@ -0,0 +1,11 @@
+local g = vim.g
+local opt = vim.opt
+
+-----------------------------------------------------------
+-- Tabs, indent
+-----------------------------------------------------------
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.softtabstop = 4
+opt.tabstop = 4
+opt.smartindent = true
