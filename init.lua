82ae41f94aea4e6766634e20b43c8d4799f60ec6
Ryan Gibson <ryan@gibs.gg>
2025-08-12 16:04:40 -0500

    Add more keybinds and changes to a few options

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
-require("config/lazy")
 require("config/options")
 require("config/keymaps")
+require("config/lazy")
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.hl.on_yank { higroup = 'Visual', timeout = 500 }
+  end,
+  group = vim.api.nvim_create_augroup('YankHighlight', { clear = true }),
+  pattern = '*',
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,6 @@
-local g = vim.g
+local g = g
 local opt = vim.opt
+local o = vim.o
 
 -----------------------------------------------------------
 -- Tabs, indent
@@ -9,3 +10,40 @@ opt.shiftwidth = 4
 opt.softtabstop = 4
 opt.tabstop = 4
 opt.smartindent = true
+
+-- Make line numbers default
+o.number = true
+
+-- Don't show the mode, since it's already in the status line
+o.showmode = false
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+o.mouse = 'a'
+
+-- Sync clipboard between OS and Neo
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  o.clipboard = 'unnamedplus'
+end)
+
+-- Enable break indent
+o.breakindent = true
+
+o.list = true
+opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+o.inccommand = 'split'
+
+-- Minimal number of screen lines to keep above and below the cursor.
+o.scrolloff = 10
+-- Show which line your cursor is on
+o.cursorline = true
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+o.confirm = true
+
