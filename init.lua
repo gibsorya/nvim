55b276235bfb86ed0b8ca8852469befb960c1c2c
Ryan Gibson <ryan@gibs.gg>
2025-08-18 21:55:08 -0400

    set colorscheme on bufenter

--- init.lua
+++ init.lua
@@ -2,10 +2,20 @@ require("config/options")
 require("config/keymaps")
 require("config/lazy")
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.hl.on_yank { higroup = 'Visual', timeout = 500 }
-  end,
-  group = vim.api.nvim_create_augroup('YankHighlight', { clear = true }),
-  pattern = '*',
+local RyanGibsonGroup = vim.api.nvim_create_augroup("RyanGibson", {})
+local YankGroup = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.hl.on_yank({ higroup = "Visual", timeout = 500 })
+	end,
+	group = YankGroup,
+	pattern = "*",
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+	callback = function()
+		pcall(vim.cmd.colorscheme, "catputccin")
+	end,
+    group = RyanGibsonGroup
 })
